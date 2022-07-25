dad96ce6a522c131a53fafbc752fe7a6aed8ff13
Julieta <valdezjulieta45@gmail.com>
2022-07-24 23:27:23 -0300

    agregue cambios al ejercicio 2 de array

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,13 +1,22 @@
 let miArray=[];
 
 do{
-    let textoIngresado = prompt('Ingrese una palabra');
+    let textoIngresado = prompt('Ingrese una ciudad');
     miArray.push(textoIngresado);
 }
-while (confirm('Quiere ingresar otra palabra?'));
+while (confirm('Quiere ingresar otra ciudad ?'));
+document.write(miArray);
+document.write('<br></br>');
+
+document.write('La longitud del arreglo es de: ' +miArray.length+ ' items');
+document.write('<br></br>');
+
+miArray.push('paris');
 document.write(miArray);
 
-
-document.write('La longitud del arreglo es de:' +miArray.length+ 'items');
 document.write('<br></br>');
-document.write(miArray[miArray.length]='paris');
+document.write(miArray[0]);
+document.write(miArray[2]);
+
+
+
